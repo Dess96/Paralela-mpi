1ce6899dac919f5916c7639c14f8dfcf1b1bd11c
Dess96 <dessalfaro@gmail.com>
2018-11-09 13:51:11 -0600

    Version 2
    *Algoritmo de Dijkstra implementado con el grafo del archivo de texto
    *Corregir codigo serial

--- Labs cortos mpi/Ejercicio 5/dijkstra.cpp
+++ Labs cortos mpi/Ejercicio 5/dijkstra.cpp
@@ -19,8 +19,6 @@
 #include <vector>
 #include <fstream>
 
-#define V 9 
-
 using namespace std;
 
 //#define DEBUG
@@ -29,11 +27,11 @@ void uso(string nombre_prog);
 
 void leeAdyacencias(ifstream& ae, vector< vector< int > >& ma, int& cntVertices);
 
-int minDistance(int dist[], bool sptSet[]);
+int minDistance(int* dist, bool* sptSet, int cntVertices);
 
-void printSolution(int dist[], int n);
+void printSolution(int* dist, int n);
 
-void dijkstra(int graph[V][V], int src);
+void dijkstra(vector<vector<int>>, int src, int cntVertices);
 
 int main(int argc, char* argv[]) {
 	int mid; // id de cada proceso
@@ -57,34 +55,18 @@ int main(int argc, char* argv[]) {
 	vector< vector< int > > matrizAdyacencias;
 	int cntVertices;
 
+	if (mid == 0) {
+		uso("Dijkstra");
+	}
+
 	if (!archivoEntrada) { // operador ! sobrecargado
 		cout << "No se pudo abrir el archivo de entrada" << endl;
 		cin.ignore();
 		return 1;
 	}
 	leeAdyacencias(archivoEntrada, matrizAdyacencias, cntVertices);
-	cout << "MATRIZ DE ADYACENCIAS" << endl;
-	for (int i = 0; i < cntVertices; i++) {
-		for (int j = 0; j < cntVertices; j++)
-			cout << matrizAdyacencias[i][j] << ',';
-		cout << endl;
-	}
 
-	if (mid == 0) {
-		uso("Dijkstra");
-	}
-	int graph[V][V] = { {0, 4, 0, 0, 0, 0, 0, 8, 0},
-					 {4, 0, 8, 0, 0, 0, 0, 11, 0},
-					 {0, 8, 0, 7, 0, 4, 0, 0, 2},
-					 {0, 0, 7, 0, 9, 14, 0, 0, 0},
-					 {0, 0, 0, 9, 0, 10, 0, 0, 0},
-					 {0, 0, 4, 14, 10, 0, 2, 0, 0},
-					 {0, 0, 0, 0, 0, 2, 0, 1, 6},
-					 {8, 11, 0, 0, 0, 0, 1, 0, 7},
-					 {0, 0, 2, 0, 0, 0, 6, 7, 0}
-	};
-
-//	dijkstra(graph, 0);
+	dijkstra(matrizAdyacencias, 0, cntVertices);
 	/* finalización de la ejecución paralela */
 	if (mid == 0)
 		cin.ignore();
@@ -131,9 +113,9 @@ void leeAdyacencias(ifstream& ae, vector< vector< int > >& ma, int& cntVertices)
 	}
 }
 
-int minDistance(int dist[], bool sptSet[]) {
+int minDistance(int* dist, bool* sptSet, int cntVertices) {
 	int min = INT_MAX, min_index;
-	for (int v = 0; v < V; v++) {
+	for (int v = 0; v < cntVertices; v++) {
 		if (sptSet[v] == false && dist[v] <= min) {
 			min = dist[v], min_index = v;
 		}
@@ -141,27 +123,27 @@ int minDistance(int dist[], bool sptSet[]) {
 	return min_index;
 }
 
-void printSolution(int dist[], int n) {
+void printSolution(int* dist, int n) {
 	printf("Vertex   Distance from Source\n");
-	for (int i = 0; i < V; i++)
+	for (int i = 0; i < n; i++)
 		printf("%d tt %d\n", i, dist[i]);
 }
 
-void dijkstra(int graph[V][V], int src) {
-	int dist[V];    
-	bool sptSet[V]; 
-	for (int i = 0; i < V; i++)
+void dijkstra(vector<vector<int>> matrizAdyacencias, int src, int cntVertices) {
+	int* dist = new int[cntVertices];    
+	bool* sptSet = new bool[cntVertices]; 
+	for (int i = 0; i < cntVertices; i++)
 		dist[i] = INT_MAX, sptSet[i] = false;
 
 	dist[src] = 0;
 
-	for (int count = 0; count < V - 1; count++) {
-		int u = minDistance(dist, sptSet);
+	for (int count = 0; count < cntVertices - 1; count++) {
+		int u = minDistance(dist, sptSet, cntVertices);
 		sptSet[u] = true;
-		for (int v = 0; v < V; v++)
-			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
-				&& dist[u] + graph[u][v] < dist[v])
-				dist[v] = dist[u] + graph[u][v];
+		for (int v = 0; v < cntVertices; v++)
+			if (!sptSet[v] && matrizAdyacencias[u][v] && dist[u] != INT_MAX
+				&& dist[u] + matrizAdyacencias[u][v] < dist[v])
+				dist[v] = dist[u] + matrizAdyacencias[u][v];
 	}
-	printSolution(dist, V);
+	printSolution(dist, cntVertices);
 }
