fe9ba88a40bbdf9570b189c7793acd77773541b1
Dess96 <dessalfaro@gmail.com>
2018-11-12 15:20:38 -0600

    Version 2
    *Generate funciona
    *Arreglar el gather del mergeSort

--- Labs cortos mpi/Ejercicio 4/merge.cpp
+++ Labs cortos mpi/Ejercicio 4/merge.cpp
@@ -1,21 +1,6 @@
-/* Archivo:      mpi_plantilla.cpp
-* Propósito:   ....
-*
-* Compilación:   mpicxx -g -Wall -o mpi_plantilla mpi_plantilla.cpp
-* Ejecución:     mpiexec -n <num_proc> ./mpi_plantilla <secuencia de valores de parámetros>
-*
-* Entradas:     ...
-* Salidas:    ...
-*
-* Notas:
-* 1.  bandera DEBUG produce salida detallada para depuración.
-*
-*/
-
 #include <mpi.h> 
 #include <iostream>
 #include<stdlib.h>
-#include<iostream>
 #include<time.h>
 #include<vector>
 #include<algorithm>
@@ -29,11 +14,12 @@ void obt_args(
 	char*    argv[]        /* in  */,
 	int&     cant  /* out */);
 
-std::vector<int> arreglo;
 void generate(int);
-void mergeSort(int, int);
+void mergeSort(int);
 void merge(int, int);
 void merge_v2(int, int, int);
+std::vector<int> arreglo;
+
 
 int main(int argc, char* argv[]) {
 	int mid; // id de cada proceso
@@ -41,10 +27,11 @@ int main(int argc, char* argv[]) {
 	int cant;
 	MPI_Status mpi_status; // para capturar estado al finalizar invocación de funciones MPI
 
-	/* Arrancar ambiente MPI */
+						   /* Arrancar ambiente MPI */
 	MPI_Init(&argc, &argv);             		/* Arranca ambiente MPI */
 	MPI_Comm_rank(MPI_COMM_WORLD, &mid); 		/* El comunicador le da valor a id (rank del proceso) */
 	MPI_Comm_size(MPI_COMM_WORLD, &cnt_proc);  /* El comunicador le da valor a p (número de procesos) */
+
 
 #  ifdef DEBUG 
 	if (mid == 0)
@@ -53,43 +40,27 @@ int main(int argc, char* argv[]) {
 #  endif
 
 	/* ejecución del proceso principal */
-	uso("MergeSort paralelo");
+	if (mid == 0) {
+		uso("MergeSort");
+	}
 	obt_args(argv, cant);
 	generate(cant);
-	mergeSort(cant, cnt_proc);
+	mergeSort(cant);
 	/* finalización de la ejecución paralela */
 	if (mid == 0)
 		cin.ignore();
-	MPI_Barrier(MPI_COMM_WORLD); // para sincronizar la finalización de los procesos
+	MPI_Barrier(MPI_COMM_WORLD); // para sincronizar la finalización de los procesos. Sincronizacion (igual a openmp)
 
 	MPI_Finalize();
 	return 0;
-}  /* main */
+}  
 
-   /*---------------------------------------------------------------------
-   * REQ: N/A
-   * MOD: N/A
-   * EFE: despliega mensaje indicando cómo ejecutar el programa y pasarle parámetros de entrada.
-   * ENTRAN:
-   *		nombre_prog:  nombre del programa
-   * SALEN: N/A
-   */
 void uso(string nombre_prog /* in */) {
-	cerr << nombre_prog.c_str() << " secuencia de parámetros de entrada" << endl;
+	cerr << nombre_prog.c_str() << " secuencia de parametros de entrada" << endl;
 	cout << "El parametro de entrada es la cantidad de numeros a ordenar" << endl;
 	cout << "La salida es la lista de numeros aleatorios ordenada por mergeSort" << endl;
-	exit(0);
 }  /* uso */
 
-   /*---------------------------------------------------------------------
-   * REQ: N/A
-   * MOD: dato_salida
-   * EFE: obtiene los valores de los argumentos pasados por "línea de comandos".
-   * ENTRAN:
-   *		nombre_prog:  nombre del programa
-   * SALEN:
-   *		dato_salida: un dato de salida con un valor de argumento pasado por "línea de comandos".
-   */
 void obt_args(
 	char*    argv[]        /* in  */,
 	int&     cant  /* out */) {
@@ -97,7 +68,7 @@ void obt_args(
 	cant = strtol(argv[1], NULL, 10); // se obtiene valor del argumento 1 pasado por "línea de comandos".
 
 #  ifdef DEBUG
-	cout << "dato_salida = " << dato_salida << endl;
+	cout << "cant = " << cant << endl;
 #  endif
 }  /* obt_args */
 
@@ -111,18 +82,30 @@ void generate(int cant) {
 	}
 }
 
-void mergeSort(int cant, int cnt_proc) {
-	int quantity = cant / cnt_proc;
-	int rango;
+void mergeSort(int cant) {
+	int* send;
+	int* rec = 0;
+	int mid, cnt_proc, tam;
+	MPI_Comm_rank(MPI_COMM_WORLD, &mid); 		/* El comunicador le da valor a id (rank del proceso) */
+	MPI_Comm_size(MPI_COMM_WORLD, &cnt_proc);  /* El comunicador le da valor a p (número de procesos) */
+	int block = cant / cnt_proc;
 	vector<int>::iterator it;
+	send = (int*)malloc(block * sizeof(int));
+	it = arreglo.begin() + (mid*block);
+	sort(it, it + block);
 	for (int i = 0; i < cant; i++) {
-		it = arreglo.begin() + (rango*quantity);
-		if (it + quantity != arreglo.end()) {
-			sort(it, it + quantity);
+		send[i] = arreglo[i];
+	}
+	tam = cnt_proc * block;
+	if (mid == 0) {
+		rec = (int*)malloc(tam * sizeof(int));;
+	}
+	MPI_Gather(send, block * 4, MPI_INT, rec, block * 4, MPI_INT, 0, MPI_COMM_WORLD);
+	if (mid == 0) {
+		for (int i = 0; i < 8;i++) {
+			cout << rec[i] << endl;
 		}
 	}
-	merge(cant, quantity);
-	merge_v2(cant, quantity, cnt_proc);
 }
 
 void merge(int cant, int quantity) {
